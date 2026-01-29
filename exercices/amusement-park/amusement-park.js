905bb60098104ba446e72c412f63018ab54cc3b8
Ahmed Al Qublan <ahmed.1522a@gmail.com>
2026-01-29 16:23:16 +0100

    chore(amusement-park): Exercice terminé [15] [DONE]

--- exercices/amusement-park/amusement-park.js
+++ exercices/amusement-park/amusement-park.js
@@ -10,7 +10,12 @@
  * @returns {Visitor} the visitor that was created
  */
 export function createVisitor(name, age, ticketId) {
-  throw new Error('Remove this line and implement the function');
+	const Visitor = {
+		name: name,
+		age: age,
+		ticketId: ticketId,
+	};
+	return Visitor;
 }
 
 /**
@@ -20,7 +25,12 @@ export function createVisitor(name, age, ticketId) {
  * @returns {Visitor} the visitor without a ticket
  */
 export function revokeTicket(visitor) {
-  throw new Error('Remove this line and implement the function');
+	if (visitor.ticketId == null) {
+		return visitor;
+	} else {
+		visitor.ticketId = null;
+		return visitor;
+	}
 }
 
 /**
@@ -31,7 +41,20 @@ export function revokeTicket(visitor) {
  * @returns {string} ticket status
  */
 export function ticketStatus(tickets, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  if (!(ticketId in tickets)) {
+    return "unknown ticket id";
+  }
+
+  const ticketOwner = tickets[ticketId];
+
+  if (ticketOwner === null) {
+    return "not sold";
+  }
+  else
+  {
+    return `sold to ${ticketOwner}`;
+  }
+
 }
 
 /**
@@ -43,7 +66,12 @@ export function ticketStatus(tickets, ticketId) {
  * @returns {string} ticket status
  */
 export function simpleTicketStatus(tickets, ticketId) {
-  throw new Error('Remove this line and implement the function');
+  const ticketOwner = tickets[ticketId];
+  if (ticketOwner) {
+    return ticketOwner;
+  } else {
+    return "invalid ticket !!!";
+  }
 }
 
 /**
@@ -53,5 +81,5 @@ export function simpleTicketStatus(tickets, ticketId) {
  * @returns {string | undefined} version
  */
 export function gtcVersion(visitor) {
-  throw new Error('Remove this line and implement the function');
+  return visitor.gtc?.version;
 }
